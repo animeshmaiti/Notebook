bc3d137e4834c87fe69e1fac4763da7ca0bb4734
Animesh Maiti <a.maiti770@gmail.com>
2024-01-05 11:09:36 +0530

    complete CRUD op

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -37,4 +37,46 @@ router.post('/addnotes', fetchuser, [
     }
 })
 
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    // create a new note object
+    try {
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        //find the note to update and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send('Not Found');
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('not allowed')
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("internal server error");
+    }
+})
+
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+        //find the note to delete and delete it
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send('Not Found');
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('not allowed')
+        }
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({ "success": "note has been deleted",note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("internal server error");
+    }
+})
 module.exports = router;
