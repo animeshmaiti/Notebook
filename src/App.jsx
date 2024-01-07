13bf45e5695f64a2f088d64d6654b06cb59b17b9
Animesh Maiti <a.maiti770@gmail.com>
2024-01-07 22:04:42 +0530

    add router and basic comp

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  const [myStyle, setMyStyle] = useState({
+    color: "white",
+    backgroundColor: "#212529",
+    btnText: "Dark Mode",
+    btn_style: "light",
+    border_style: "secondary",
+    svg_style: "rgba(222, 226, 230, 0.75)",
+  });
+  useEffect(() => {
+    document.body.style.backgroundColor = myStyle.color === 'white' ? '#212529' : 'white';
+    document.body.style.color = myStyle.color === 'white' ? 'white' : '#212529';
+    document.documentElement.setAttribute("data-bs-theme", myStyle.color === 'white'? 'dark':'light');
+  }, [myStyle.color]);
+
+  const toggleStyle = () => {
+    if (myStyle.color === "white") {
+      setMyStyle(
+        //async
+        {
+          color: "#212529",
+          backgroundColor: "white",
+          btnText: "Light Mode",
+          btn_style: "dark",
+          svg_style: "rgba(67, 68, 68, 0.75)",
+        }
+      );
+    } else {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "#212529",
+        btnText: "Dark Mode",
+        nav_bg: "navbar navbar-expand-lg bg-dark",
+        btn_style: "light",
+        svg_style: "rgba(222, 226, 230, 0.75)",
+      });
+    }
+  };
+  return (
+    <>
+    <Router>
+      <Navbar myTheme={myStyle} toggleStyle={toggleStyle} />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
